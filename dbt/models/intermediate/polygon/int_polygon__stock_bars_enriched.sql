474abb7392b86ff3bbf6977429bfdd6d8470bce8
Dave Anaya <danaya2846@gmail.com>
2025-09-25 16:26:11 -0500

    updaye dbt files with proper file names

--- dbt/models/intermediate/polygon/int_polygon__stock_bars_enriched.sql
+++ dbt/models/intermediate/polygon/int_polygon__stock_bars_enriched.sql
@@ -6,28 +6,18 @@
     )
 }}
 
-with stg_stock_bars as (
-    select * from {{ ref('stg_polygon__stock_bars_daily') }}
+with source as (
+    select * from {{ ref('stg_polygon__stock_bars_casted') }}
 ),
 
 enriched as (
 select
--- Passthrough columns
-stock_bar_id,
-ticker,
-trade_date,
-loaded_at,
-open_price,
-high_price,
-low_price,
-close_price,
-volume,
-transactions,
-
--- Calculated metrics
-avg(close_price) over (partition by ticker order by trade_date rows between 19 preceding and current row) as moving_avg_20d,
-avg(close_price) over (partition by ticker order by trade_date rows between 49 preceding and current row) as moving_avg_50d
-from stg_stock_bars
+ t1.*,
+ avg(close_price) over (partition by ticker order by trade_date rows between 19 preceding and current row) as moving_avg_20d,
+ avg(close_price) over (partition by ticker order by trade_date rows between 49 preceding and current row) as moving_avg_50d,
+ (close_price - lag(close_price, 1) over (partition by ticker order by trade_date)) as price_change_1d,
+ (high_price - low_price) as daily_price_range
+from source t1
 
 {% if is_incremental() %}
 -- this filter will limit the data scanned to only the new records
--- dbt/models/marts/polygon/fct_polygon__stock_bars_performance.sql
+++ dbt/models/marts/polygon/fct_polygon__stock_bars_performance.sql
@@ -1,5 +1,3 @@
--- dbt/models/marts/mart_polygon__daily_stock_performance.sql
-
 {{
     config(
         materialized='incremental',
@@ -8,31 +6,8 @@
     )
 }}
 
-with enriched_prices as (
-    select * from {{ ref('int_polygon__stock_prices_enriched') }}
+with source as (
+    select * from {{ ref('int_polygon__stock_bars_enriched') }}
 ),
 
-final as (
-select
--- Identifiers
-stock_bar_id,
-ticker,
-trade_date,
--- Performance Metrics
-close_price,
-moving_avg_20d,
-moving_avg_50d,
-(close_price - lag(close_price, 1) over (partition by ticker order by trade_date)) as price_change_1d,
-
--- Volume and Volatility
-volume,
-(high_price - low_price) as daily_price_range
-from enriched_prices
-{% if is_incremental() %}
--- Assuming 'loaded_at' is available from the intermediate model.
--- If not, you could also filter on 'trade_date'.
-where loaded_at > (select max(loaded_at) from {{ this }})
-{% endif %}
-)
-
-select * from final
+select * from source
