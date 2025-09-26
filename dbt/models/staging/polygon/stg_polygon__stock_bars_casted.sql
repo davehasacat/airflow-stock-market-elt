12769f77e77e8374b289612ad1eb9f76ca423df2
Dave Anaya <danaya2846@gmail.com>
2025-09-26 16:12:22 -0500

    update syntax. update marts schema

--- dbt/models/staging/polygon/stg_polygon__stock_bars_casted.sql
+++ dbt/models/staging/polygon/stg_polygon__stock_bars_casted.sql
@@ -4,17 +4,17 @@ with source as (
 
 renamed_and_casted as (
 select
-ticker || '_' || trade_date as stock_bar_id,    -- use as primary key
-ticker,
-trade_date,
-inserted_at as loaded_at,
-open as open_price,
-high as high_price,
-low as low_price,
-close as close_price,
-vwap as volume_weighted_average_price,
-volume,
-transactions
+  ticker || '_' || trade_date as stock_bar_id,    -- use as primary key
+  ticker,
+  trade_date,
+  inserted_at as loaded_at,
+  open as open_price,
+  high as high_price,
+  low as low_price,
+  close as close_price,
+  vwap as volume_weighted_average_price,
+  volume,
+  transactions
 from source
 )
 
