8e54a9249f4ab1cbb2fed5addd014256d5034c2b
Dave Anaya <danaya2846@gmail.com>
2025-09-27 01:04:04 -0500

    clean up case statements

--- dbt/models/intermediate/polygon/int_polygon__stock_bars_enriched.sql
+++ dbt/models/intermediate/polygon/int_polygon__stock_bars_enriched.sql
@@ -27,6 +27,7 @@ select
   volume_weighted_average_price,
   avg(close_price) over (partition by ticker order by trade_date rows between 19 preceding and current row) as moving_avg_20d,
   avg(close_price) over (partition by ticker order by trade_date rows between 49 preceding and current row) as moving_avg_50d,
+  avg(close_price) over (partition by ticker order by trade_date rows between 119 preceding and current row) as moving_avg_120d,
   (close_price - lag(close_price, 1) over (partition by ticker order by trade_date)) as price_change_1d,
   (high_price - low_price) as daily_price_range,
   volume,
@@ -40,19 +41,14 @@ select
   stock_bar_id,
   ticker,
   trade_date,
-  open_price,
-  high_price,
-  low_price,
-  close_price,
-  volume_weighted_average_price,
-  case
-    when moving_avg_20d >= 1.0000 then round(moving_avg_20d, 2)
-    else round(moving_avg_20d, 4)
-  end as moving_avg_20d,
-  case
-    when moving_avg_50d >= 1.0000 then round(moving_avg_50d, 2)
-    else round(moving_avg_50d, 4)
-  end as moving_avg_50d,
+  round(open_price, case when open_price >= 1 then 2 else 4 end) as open_price,
+  round(high_price, case when high_price >= 1 then 2 else 4 end) as high_price,
+  round(low_price, case when low_price >= 1 then 2 else 4 end) as low_price,
+  round(close_price, case when close_price >= 1 then 2 else 4 end) as close_price,
+  round(volume_weighted_average_price, case when volume_weighted_average_price >= 1 then 2 else 4 end) as volume_weighted_average_price,
+  round(moving_avg_20d, case when moving_avg_20d >= 1 then 2 else 4 end) as moving_avg_20d,
+  round(moving_avg_50d, case when moving_avg_50d >= 1 then 2 else 4 end) as moving_avg_50d,
+  round(moving_avg_120d, case when moving_avg_120d >= 1 then 2 else 4 end) as moving_avg_120d,
   price_change_1d,
   daily_price_range,
   volume,
