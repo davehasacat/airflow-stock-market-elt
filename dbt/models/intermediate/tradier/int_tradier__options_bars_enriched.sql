d1d2d7213f8a54f5d9091370f2eda52d17e876c4
Dave Anaya <danaya2846@gmail.com>
2025-10-06 12:16:54 -0500

    add stg and int options models

---
+++ dbt/models/intermediate/tradier/int_tradier__options_bars_enriched.sql
@@ -0,0 +1,36 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='option_bar_id',
+        incremental_strategy='delete+insert'
+    )
+}}
+
+with source as (
+    select * from {{ ref('stg_tradier__options_bars_casted') }}
+    {% if is_incremental() %}
+        where loaded_at > (select max(loaded_at) from {{ this }})
+    {% endif %}
+),
+
+add_calculated_metrics as (
+select
+  option_bar_id,
+  option_symbol,
+  underlying_ticker,
+  trade_date,
+  expiration_date,
+  strike_price,
+  option_type,
+  open_price,
+  high_price,
+  low_price,
+  close_price,
+  volume,
+  volume_weighted_average_price,
+  loaded_at,
+  {{ dbt.date_diff('trade_date', 'expiration_date', 'day') }} as days_to_expiration
+from source
+)
+
+select * from add_calculated_metrics
