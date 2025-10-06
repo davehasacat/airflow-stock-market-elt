5eabd32d653818c2c138630a2eddd0a2f157da6b
Dave Anaya <danaya2846@gmail.com>
2025-10-06 14:33:53 -0500

    update polygon ingest start date

--- dags/polygon/stocks_polygon_ingest.py
+++ dags/polygon/stocks_polygon_ingest.py
@@ -14,7 +14,7 @@ from dags.utils.polygon_datasets import S3_MANIFEST_DATASET
 # Define the DAG
 @dag(
     dag_id="stocks_polygon_ingest",
-    start_date=pendulum.datetime(2025, 1, 1, tz="UTC"),
+    start_date=pendulum.datetime(2025, 10, 1, tz="UTC"),
     schedule="0 0 * * 1-5",
     catchup=True,
     tags=["ingestion", "polygon"],
